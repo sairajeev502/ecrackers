f26b97c5144b4f830014af32936ace61a6e040b9
Dhanush Adithya <s.dhanushadithya@gmail.com>
2021-10-14 09:58:24 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 
 const app = express();
 
@@ -14,9 +14,9 @@ app.set("view engine", "ejs");
 //const dbURI = process.env.MONGODB_URI
 //mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify: false })
 //.then(_ =>
-//app.listen(PORT, () => {
-//console.log(`Server is running on port ${PORT}`);
-//})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 //)
 //.catch(err => console.log(err))
 
