8d6846ea1c45a1d01b26019ae065204efd1c1c29
Dhanush Adithya <s.dhanushadithya@gmail.com>
2021-10-21 11:53:23 +0530

    Added add to cart feature

--- models/User.js
+++ models/User.js
@@ -11,6 +11,10 @@ const userSchema = new mongoose.Schema({
   lastName: String,
   email: strRequired,
   password: strRequired,
+  orders: {
+    type: [String],
+    default: [],
+  },
 });
 
 userSchema.pre("save", async function (next) {
