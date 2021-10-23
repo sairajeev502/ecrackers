f1522708fcbfb0bbe89870d0d2802574a253e83a
Dhanush Adithya <s.dhanushadithya@gmail.com>
2021-10-23 21:10:25 +0530

    Removed merge markers

--- index.js
+++ index.js
@@ -1,43 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require("./routes/authRoutes");
 const {
-	checkUser,
-	localProducts,
-	userCartItems
-} = require('./middlewares/authMiddleware');
+  checkUser,
+  localProducts,
+  //userCartItems,
+} = require("./middlewares/authMiddleware");
 const app = express();
 
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 5721cfc9bea4198bd1f40948a5221f73786c8dc1
 const PORT = process.env.PORT || 3000;
-
-
-
+const dbURI = process.env.MONGODB_URI;
 
 app.use(express.static("public"));
-<<<<<<< HEAD
-=======
-app.use(express.static('public'));
->>>>>>> 93aedfa4a81df0d335f831196879f8ad843482f0
-=======
->>>>>>> 5721cfc9bea4198bd1f40948a5221f73786c8dc1
 app.use(express.json());
 app.use(cookieParser());
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-const dbURI = process.env.MONGODB_URI;
 mongoose
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 5721cfc9bea4198bd1f40948a5221f73786c8dc1
   .connect(dbURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -48,68 +31,48 @@ mongoose
     })
   )
   .catch((err) => console.log(err));
-app.get("/", checkUser,localProducts, (_, res) => res.render("index"));
+
+app.get("/", checkUser, localProducts, (_, res) => res.render("index"));
 app.get("/about-us", (_, res) => res.render("about-us"));
 app.get("/contact", checkUser, (_, res) => res.render("contact"));
 app.get("/faq", (_, res) => res.render("faq"));
 app.get("/index-3", (_, res) => res.render("index-3"));
-<<<<<<< HEAD
-=======
-	.connect(dbURI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	})
-	.then(() => console.log('DATABASE CONNECTED'))
-	.catch((err) => console.log(err));
 
-app.get('/', checkUser, localProducts, (_, res) => res.render('index'));
-app.get('/about-us', (_, res) => res.render('about-us'));
-app.get('/contact', checkUser, (_, res) => res.render('contact'));
-app.get('/faq', (_, res) => res.render('faq'));
-app.get('/index-3', (_, res) => res.render('index-3'));
->>>>>>> 93aedfa4a81df0d335f831196879f8ad843482f0
-=======
->>>>>>> 5721cfc9bea4198bd1f40948a5221f73786c8dc1
+app.get("/login-login", checkUser, (_, res) => res.render("login-login"));
+app.get("/login-register", (_, res) => res.render("login-register"));
+app.get("/vendor-login", (_, res) => res.render("vendor-login"));
+app.get("/vendor-register", (_, res) => res.render("vendor-register"));
+app.get("/admin-login", (_, res) => res.render("admin-login"));
+app.get("/my-profile", (_, res) => res.render("my-profile"));
 
-app.get('/login-login', checkUser, (_, res) => res.render('login-login'));
-app.get('/login-register', (_, res) => res.render('login-register'));
-app.get('/vendor-login', (_, res) => res.render('vendor-login'));
-app.get('/vendor-register', (_, res) => res.render('vendor-register'));
-app.get('/admin-login', (_, res) => res.render('admin-login'));
-app.get('/my-profile', (_, res) => res.render('my-profile'));
+app.get("/vendor-dashboard", (_, res) => res.render("vendorDashboard")); //vendor Dashboard
 
-app.get('/vendor-dashboard', (_, res) => res.render('vendorDashboard')); //vendor Dashboard
-
-app.get('/blog-3-column', (_, res) => res.render('blog-3-column'));
-app.get('/blog-audio-format', (_, res) => res.render('blog-audio-format'));
-app.get('/blog-details-left-sidebar', (_, res) =>
-	res.render('blog-details-left-sidebar')
+app.get("/blog-3-column", (_, res) => res.render("blog-3-column"));
+app.get("/blog-audio-format", (_, res) => res.render("blog-audio-format"));
+app.get("/blog-details-left-sidebar", (_, res) =>
+  res.render("blog-details-left-sidebar")
 );
-app.get('/blog-gallery-format', (_, res) => res.render('blog-gallery-format'));
-app.get('/blog-left-sidebar', (_, res) => res.render('blog-left-sidebar'));
-app.get('/blog-list', (_, res) => res.render('blog-list'));
-app.get('/blog-list-left-sidebar', (_, res) =>
-	res.render('blog-list-left-sidebar')
+app.get("/blog-gallery-format", (_, res) => res.render("blog-gallery-format"));
+app.get("/blog-left-sidebar", (_, res) => res.render("blog-left-sidebar"));
+app.get("/blog-list", (_, res) => res.render("blog-list"));
+app.get("/blog-list-left-sidebar", (_, res) =>
+  res.render("blog-list-left-sidebar")
 );
-app.get('/blog-video-format', (_, res) => res.render('blog-video-format'));
+app.get("/blog-video-format", (_, res) => res.render("blog-video-format"));
 
-app.get('/checkout', (_, res) => res.render('checkout'));
-app.get('/compare', (_, res) => res.render('compare'));
-app.get('/wishlist', (_, res) => res.render('wishlist'));
-app.get('/shop-left-sidebar', checkUser, localProducts, (_, res) =>
-	res.render('shop-left-sidebar')
+app.get("/checkout", (_, res) => res.render("checkout"));
+app.get("/compare", (_, res) => res.render("compare"));
+app.get("/wishlist", (_, res) => res.render("wishlist"));
+app.get("/shop-left-sidebar", checkUser, localProducts, (_, res) =>
+  res.render("shop-left-sidebar")
 );
-app.get('/shop-list', (_, res) => res.render('shop-list'));
-app.get('/shopping-cart', (_, res) => res.render('shopping-cart'));
+app.get("/shop-list", (_, res) => res.render("shop-list"));
+app.get("/shopping-cart", (_, res) => res.render("shopping-cart"));
 
-app.get('/404', (_, res) => res.render('404'));
-app.get('/single-product-sale', (_, res) => res.render('single-product-sale'));
-app.get('/single-product-tab-style-top', (_, res) =>
-	res.render('single-product-tab-style-top')
+app.get("/404", (_, res) => res.render("404"));
+app.get("/single-product-sale", (_, res) => res.render("single-product-sale"));
+app.get("/single-product-tab-style-top", (_, res) =>
+  res.render("single-product-tab-style-top")
 );
 
 app.use(authRoutes);
-
-app.listen(process.env.PORT, () => {
-	console.log(`Server is running on port ${process.env.PORT}`);
-});
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,202 +1,173 @@
-const { Router } = require('express');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-const { products } = require('../models/Product');
-const { checkUser } = require('../middlewares/authMiddleware');
+const { Router } = require("express");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const { products } = require("../models/Product");
+const { checkUser } = require("../middlewares/authMiddleware");
 
 const router = Router();
 
 const handleErrors = (err) => {
-	let errors = { email: '', password: '' };
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
-	if (err.message === 'Incorrect Email') {
-		errors.email = err.message;
-		return errors;
-	}
-
->>>>>>> 5721cfc9bea4198bd1f40948a5221f73786c8dc1
-
-
+  let errors = { email: "", password: "" };
 
   if (err.message === "Incorrect Email") {
     errors.email = err.message;
     return errors;
   }
-<<<<<<< HEAD
-=======
-	if (err.message === 'Incorrect Email') {
-		errors.email = err.message;
-		return errors;
-	}
->>>>>>> 93aedfa4a81df0d335f831196879f8ad843482f0
-=======
+  if (err.message === "Incorrect Password") {
+    errors.password = err.message;
+    return errors;
+  }
 
->>>>>>> 5721cfc9bea4198bd1f40948a5221f73786c8dc1
-
-	if (err.message === 'Incorrect Password') {
-		errors.password = err.message;
-		return errors;
-	}
-
-	return errors;
+  return errors;
 };
 
 const maxAge = 3 * 24 * 60 * 60;
 
 const createToken = (id) =>
-	jwt.sign({ id }, 'ecrackers digitran', {
-		expiresIn: maxAge
-	});
+  jwt.sign({ id }, "ecrackers digitran", {
+    expiresIn: maxAge,
+  });
 
 const register_post = async (req, res) => {
-	try {
-		const user = await User.create(req.body);
-		res.status(201).json({ user });
-	} catch (err) {
-		res.status(400).json({ err });
-	}
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json({ user });
+  } catch (err) {
+    res.status(400).json({ err });
+  }
 };
 
 const login_post = async (req, res) => {
-	const { email, password } = req.body;
+  const { email, password } = req.body;
 
-	try {
-		const user = await User.login(email, password);
-		if (user) {
-			const token = createToken(user._id);
-			res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
-			res.status(200).json({ user: user._id });
-			console.log({ user: user._id });
-		} else {
-			throw Error('Wrong credentials');
-		}
-	} catch (err) {
-		const errors = handleErrors(err);
-		res.status(400).json({ err: errors });
-	}
+  try {
+    const user = await User.login(email, password);
+    if (user) {
+      const token = createToken(user._id);
+      res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+      res.status(200).json({ user: user._id });
+      console.log({ user: user._id });
+    } else {
+      throw Error("Wrong credentials");
+    }
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ err: errors });
+  }
 };
 
 // wishlistdetails
 
 const wishlistdetails = async (req, res) => {
-	const { _id } = req.body;
+  const { _id } = req.body;
 
-	try {
-		const user = await User.findById(_id);
-		const count = user.wishlist.reduce((acc, val) => {
-			acc[val] = acc[val] + 1 || 1;
-			return acc;
-		}, {});
-		const keys = Object.keys(count);
-		const wishlist = keys.map((e) => {
-			const det = products.filter((product) => product.id === e)[0];
-			det.count = count[e];
-			return det;
-		});
+  try {
+    const user = await User.findById(_id);
+    const count = user.wishlist.reduce((acc, val) => {
+      acc[val] = acc[val] + 1 || 1;
+      return acc;
+    }, {});
+    const keys = Object.keys(count);
+    const wishlist = keys.map((e) => {
+      const det = products.filter((product) => product.id === e)[0];
+      det.count = count[e];
+      return det;
+    });
 
-		res.status(200).json({ wishlist });
-	} catch (e) {
-		res.status(400).json({ e });
-	}
+    res.status(200).json({ wishlist });
+  } catch (e) {
+    res.status(400).json({ e });
+  }
 };
 
 // wishlistupdate
 
 const wishlistUpdate = async (req, res) => {
-	const { id, uid: _id } = req.body;
+  const { id, uid: _id } = req.body;
 
-	try {
-		const user = await User.findById(_id);
-		const wishlist = [...user.wishlist, id].filter(Boolean);
-		await User.findOneAndUpdate({ _id }, { wishlist });
-		res.status(200).json(wishlist);
-	} catch (e) {
-		console.log({ e });
-		res.status(400).json({ e });
-	}
+  try {
+    const user = await User.findById(_id);
+    const wishlist = [...user.wishlist, id].filter(Boolean);
+    await User.findOneAndUpdate({ _id }, { wishlist });
+    res.status(200).json(wishlist);
+  } catch (e) {
+    console.log({ e });
+    res.status(400).json({ e });
+  }
 };
 
 // cartDetails
 
 const cartDetails = async (req, res) => {
-	const { _id } = req.body;
-	if (_id) {
-		try {
-			const user = await User.findById(_id);
-			const count = user.orders.reduce((acc, val) => {
-				acc[val] = acc[val] + 1 || 1;
-				return acc;
-			}, {});
-			const keys = Object.keys(count);
-			const orders = keys.map((e) => {
-				const det = products.filter((product) => product.id === e)[0];
-				det.count = count[e];
-				return det;
-			});
+  const { _id } = req.body;
+  if (_id) {
+    try {
+      const user = await User.findById(_id);
+      const count = user.orders.reduce((acc, val) => {
+        acc[val] = acc[val] + 1 || 1;
+        return acc;
+      }, {});
+      const keys = Object.keys(count);
+      const orders = keys.map((e) => {
+        const det = products.filter((product) => product.id === e)[0];
+        det.count = count[e];
+        return det;
+      });
 
-			res.status(200).json({ orders });
-		} catch (e) {
-			res.status(400).json({ e });
-		}
-	} else {
-		return null;
-	}
+      res.status(200).json({ orders });
+    } catch (e) {
+      res.status(400).json({ e });
+    }
+  } else {
+    return null;
+  }
 };
 
 // cartUpdate
 
 const cartUpdate = async (req, res) => {
-	const { id, uid: _id } = req.body;
+  const { id, uid: _id } = req.body;
 
-	try {
-		const user = await User.findById(_id);
-		const orders = [...user.orders, id].filter(Boolean);
-		await User.findOneAndUpdate({ _id }, { orders });
-		res.status(200).json(orders);
-	} catch (e) {
-		console.log({ e });
-		res.status(400).json({ e });
-	}
+  try {
+    const user = await User.findById(_id);
+    const orders = [...user.orders, id].filter(Boolean);
+    await User.findOneAndUpdate({ _id }, { orders });
+    res.status(200).json(orders);
+  } catch (e) {
+    console.log({ e });
+    res.status(400).json({ e });
+  }
 };
 
 const cartItemDelete = async (req, res) => {
-	const { id, _id } = req.body;
-	if (_id) {
-		try {
-			const user = await User.findById(_id);
-			const orders = user.orders.filter((e) => id !== e);
-			await User.findOneAndUpdate({ _id }, { orders });
-			res.status(200).json({ log: 'success' });
-		} catch (e) {
-			console.log({ e });
-			res.status(400).json({ e });
-		}
-	} else {
-		res.send(null);
-	}
+  const { id, _id } = req.body;
+  if (_id) {
+    try {
+      const user = await User.findById(_id);
+      const orders = user.orders.filter((e) => id !== e);
+      await User.findOneAndUpdate({ _id }, { orders });
+      res.status(200).json({ log: "success" });
+    } catch (e) {
+      console.log({ e });
+      res.status(400).json({ e });
+    }
+  } else {
+    res.send(null);
+  }
 };
+
 //shop items
-router.get('/single-product/:id', checkUser, (req, res) => {
-	//console.log("Id is" + req.params.id );
-	var findProduct = products.filter((product) => {
-		if (product.id === req.params.id) {
-			return product;
-		} else {
-			return;
-		}
-	});
-	//console.log(findProduct);
-	res.render('single-product', { product: findProduct });
+router.get("/single-product/:id", checkUser, (req, res) => {
+  const findProduct = products.filter(
+    (product) => product.id === req.params.id
+  );
+  res.render("single-product", { product: findProduct });
 });
 
-router.post('/register', register_post);
-router.post('/login', login_post);
-router.post('/cartUpdate', cartUpdate);
-router.post('/cartDetails', cartDetails);
-router.post('/remove', cartItemDelete);
+router.post("/register", register_post);
+router.post("/login", login_post);
+router.post("/cartUpdate", cartUpdate);
+router.post("/cartDetails", cartDetails);
+router.post("/remove", cartItemDelete);
 
 module.exports = router;
